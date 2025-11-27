7d6dc10f02d32f851bf2c32af24262728692183d
Yanina-Unrein <unrein.syanina@gmail.com>
2025-11-26 23:20:41 -0300

    cambios ui, conexion con backend, login funcional

---
+++ crm-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://servidor-6om6.onrender.com'
+};
---
+++ crm-frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://servidor-6om6.onrender.com'
+};
