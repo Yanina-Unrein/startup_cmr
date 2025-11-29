11f51cd41157a3e95ea5600d5dc7dd94aa69af5d
Yanina-Unrein <unrein.syanina@gmail.com>
2025-11-29 14:20:07 -0300

    formulario responsivo, pagina principal, p y m funcionando

---
+++ crm-frontend/src/app/features/landing/landing/landing.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  templateUrl: './landing.html',
+  styleUrl: './landing.css',
+})
+export default class Landing {
+ constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+}
